07d7b1e6b93647de903acc2e5bb40e3500494465
adrianblp <adrianblp@gmail.com>
2015-12-27 02:40:36 +0100

    Add ButterKnife

--- app/src/main/java/co/adrianblan/lightly/MainActivity.java
+++ app/src/main/java/co/adrianblan/lightly/MainActivity.java
@@ -3,52 +3,60 @@ package co.adrianblan.lightly;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
+import butterknife.BindColor;
+import butterknife.BindDrawable;
+import butterknife.ButterKnife;
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 1;
+    @Bind(R.id.serviceFab) FloatingActionButton serviceFab;
 
+    @BindDrawable(R.drawable.ic_play_arrow_white_36dp) Drawable playDrawable;
+    @BindDrawable(R.drawable.ic_pause_white_36dp) Drawable pauseDrawable;
+
+    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 1;
     private boolean isOverlayServiceActive = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final FloatingActionButton serviceFab = (FloatingActionButton)  findViewById(R.id.serviceFab);
-        serviceFab.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                if(hasDrawOverlayPermission()) {
-
-                    if(!isOverlayServiceActive()) {
-                        startOverlayService();
-
-                        serviceFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.primary_material_dark)));
-                        serviceFab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_pause_white_36dp));
-                    } else {
-                        stopOverlayService();
-
-                        serviceFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.accent_material_dark)));
-                        serviceFab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_play_arrow_white_36dp));
-                    }
-                }
-            }
-        });
+        ButterKnife.bind(this);
 
         // We request permissions, if we don't have them
         if(!hasDrawOverlayPermission()) {
             requestDrawOverlayPermission();
+        }
+    }
+
+
+    /**
+     * Called when the service FAB is clicked. Toggles the overlay service, and changes the FAB
+     * visually to indicate the change.
+     */
+    @OnClick(R.id.serviceFab)
+    public void onClick(){
+
+        if(hasDrawOverlayPermission()) {
+
+            if(!isOverlayServiceActive()) {
+                startOverlayService();
+                serviceFab.setImageDrawable(pauseDrawable);
+            } else {
+                stopOverlayService();
+                serviceFab.setImageDrawable(playDrawable);
+            }
         }
     }
 
